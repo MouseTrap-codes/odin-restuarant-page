b6d82c991d6d8379a0b8a780a816f734c52a4e42
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-07-11 15:42:08 -0500

    Create menu and contact pages

--- src/homepage.js
+++ src/homepage.js
@@ -1,29 +1,34 @@
+// ./src/homepage.js
+import bgImageSrc from "./vadim-bogulov-veSRX0sZDpQ-unsplash.jpg"
 export function homePage() {
+
+    const content = document.querySelector("#content");
+
     // bg image
-    const bgImage = document.createElement(img);
-    bgImage.src = "./vadim-bogulov-veSRX0sZDpQ-unsplash.jpg";
-    bgImage.alt = "ominous room";
+    content.style.backgroundImage = `url(${bgImageSrc})`;
+    content.style.backgroundSize = "cover";
+    content.style.backgroundPosition = "center";
+    content.style.backgroundRepeat = "no-repeat";
+
 
     // restaurant name
-    const restaurantName = document.createElement(div);
+    const restaurantName = document.createElement("div");
     restaurantName.classList.add("restaurant-name");
     restaurantName.textContent = "The Void";
 
     // subtext1
-    const subtext1 = document.createElement(div);
+    const subtext1 = document.createElement("div");
     subtext1.classList.add("subtext");
     subtext1.textContent = "Experience nothing. Taste everything. We serve the essence of simplicity.";
     
 
     // subtext2
-    const subtext2 = document.createElement(div);
+    const subtext2 = document.createElement("div");
     subtext2.classList.add("subtext");
     subtext2.textContent = "Our philosophy: Less is more. Much less if much more. Nothing is everything.";
 
 
     // append everything to content div
-    const content = document.querySelector(".content");
-    content.appendChild(bgImage);
     content.appendChild(restaurantName);
     content.appendChild(subtext1);
     content.appendChild(subtext2);
